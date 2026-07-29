98d47d819ceba013ffe80533c02ceaa43b0a5347
FoxPink-dev <aduy000@gmail.com>
2026-07-29 16:49:16 +0700

    fix: move _post_init_hook to __init__.py, remove hooks.py

--- attachment_optimizer/__init__.py
+++ attachment_optimizer/__init__.py
@@ -1,4 +1,14 @@
 from . import models
 from . import wizard
-from . import hooks
+
+
+def _post_init_hook(cr, registry):
+    from odoo import api, SUPERUSER_ID
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    group = env.ref('attachment_optimizer.group_storage_optimization_manager', raise_if_not_found=False)
+    if group:
+        users = env.ref('base.group_user')
+        group.implied_ids = [(4, users.id)]
+        admin = env.ref('base.user_admin')
+        group.users = [(4, admin.id)]
 
