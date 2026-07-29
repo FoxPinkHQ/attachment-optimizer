8157a1e1e71c33215fc88f23ed1c4ea5173fe86d
FoxPink-dev <aduy000@gmail.com>
2026-07-29 16:52:05 +0700

    fix: update _post_init_hook signature to Odoo 19 (env only)

--- attachment_optimizer/__init__.py
+++ attachment_optimizer/__init__.py
@@ -2,9 +2,7 @@ from . import models
 from . import wizard
 
 
-def _post_init_hook(cr, registry):
-    from odoo import api, SUPERUSER_ID
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def _post_init_hook(env):
     group = env.ref('attachment_optimizer.group_storage_optimization_manager', raise_if_not_found=False)
     if group:
         users = env.ref('base.group_user')
