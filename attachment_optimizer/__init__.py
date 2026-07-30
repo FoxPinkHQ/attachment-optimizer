f9b93d0626800e26ca477b02e9a87cf737cce6bf
FoxPink-dev <aduy000@gmail.com>
2026-07-30 14:40:47 +0700

    fix: Odoo 19 compat - remove category_id, use user_ids, post_init_hook

--- attachment_optimizer/__init__.py
+++ attachment_optimizer/__init__.py
@@ -1,2 +1,11 @@
 from . import models
 from . import wizard
+
+
+def _post_init_hook(env):
+    group = env.ref('attachment_optimizer.group_storage_optimization_manager', raise_if_not_found=False)
+    if group:
+        base_user = env.ref('base.group_user')
+        group.implied_ids = [(4, base_user.id)]
+        admin = env.ref('base.user_admin')
+        group.user_ids = [(4, admin.id)]
